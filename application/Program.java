0531eccdd1e774795012e0349f81ef290eaaa59d
Matheus Medeiros <mathmed22@gmail.com>
2022-05-18 15:45:01 -0300

    Sobreposicao, super e override

--- application/Program.java
+++ application/Program.java
@@ -8,29 +8,18 @@ public class Program {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Account acc = new Account(1001, "Alex", 0.0);
-		BusinessAccount bacc = new BusinessAccount(1002, "Maria", 0.0, 500.0);
+		Account acc = new Account(1001, "Alex", 1000.0);
+		acc.withdraw(200.0);
+		System.out.println(acc.getBalance());
 		
-		//UPCASTING
-		Account acc1 = bacc;
-		Account acc2 = new BusinessAccount(1003, "João", 0.0, 200.0);
-		Account acc3 = new SavingsAccount(1004, "Jorge", 0.0, 0.01);
+		Account acc1 = new SavingsAccount(1002, "Ana", 1000.0, 0.01);
+		acc1.withdraw(200.0);
+		System.out.println(acc1.getBalance());
 		
-		//DOWNCASTING
+		Account acc2 = new BusinessAccount(1003, "Jorge", 1000.0, 200.0);
+		acc2.withdraw(200.0);
+		System.out.println(acc2.getBalance());
 		
-		BusinessAccount acc4 = (BusinessAccount)acc2;
-		acc4.loan(100.0);
-		
-		if(acc3 instanceof BusinessAccount) {
-			BusinessAccount acc5 = (BusinessAccount)acc3;
-			acc5.loan(200.0);
-		}
-
-		if(acc3 instanceof SavingsAccount) {
-			SavingsAccount acc5 = (SavingsAccount)acc3;
-			acc5.updateBalance();
-			System.out.println("Update");
-		}
 	}
 }
 
--- entities/BusinessAccount.java
+++ entities/BusinessAccount.java
@@ -28,4 +28,10 @@ public class BusinessAccount extends Account {
 		//deposit(amount);
 		}
 	}
+	
+	@Override
+	public void withdraw(double amount) {
+		super.withdraw(amount);
+		balance -= 2.0;
+	}
 }
