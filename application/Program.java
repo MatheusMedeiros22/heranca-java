ec30b9b0e4acb434d4014bf8dc051bdbf861de47
Matheus Medeiros <mathmed22@gmail.com>
2022-05-18 15:21:39 -0300

    Upcasting e Downcasting

--- application/Program.java
+++ application/Program.java
@@ -2,18 +2,36 @@ package application;
 
 import entities.Account;
 import entities.BusinessAccount;
+import entities.SavingsAccount;
 
 public class Program {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Account account = new Account(3201, "Matheus Medeiros", 0.0);
-		BusinessAccount bAccount = new BusinessAccount(3202, "Fernanda", 100000.0, 10000.0); 
+		Account acc = new Account(1001, "Alex", 0.0);
+		BusinessAccount bacc = new BusinessAccount(1002, "Maria", 0.0, 500.0);
 		
+		//UPCASTING
+		Account acc1 = bacc;
+		Account acc2 = new BusinessAccount(1003, "João", 0.0, 200.0);
+		Account acc3 = new SavingsAccount(1004, "Jorge", 0.0, 0.01);
 		
-		System.out.println(account); //Sem o toString imprime o endereço de memoria
-		System.out.println(account.getHolder());
-		System.out.println(bAccount.getHolder());
-	}
+		//DOWNCASTING
+		
+		BusinessAccount acc4 = (BusinessAccount)acc2;
+		acc4.loan(100.0);
+		
+		if(acc3 instanceof BusinessAccount) {
+			BusinessAccount acc5 = (BusinessAccount)acc3;
+			acc5.loan(200.0);
+		}
 
+		if(acc3 instanceof SavingsAccount) {
+			SavingsAccount acc5 = (SavingsAccount)acc3;
+			acc5.updateBalance();
+			System.out.println("Update");
+		}
+	}
 }
+
+	
